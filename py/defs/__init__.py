367278d9523e5798839560e4c71a7b564ddff74a
edelvarden <edelvarden@gmail.com>
2024-09-22 22:44:25 +0300

    fix: node loading

--- py/defs/__init__.py
+++ py/defs/__init__.py
@@ -11,7 +11,7 @@ for module_path in glob.glob(dir_name + "/ext/*.py"):
     module_name = os.path.basename(module_path)
     module_name = os.path.splitext(module_name)[0]
     package_name = (
-        f"custom_nodes.ComfyUI-SaveImageWithMetaData.py.defs.ext.{module_name}"
+        f"custom_nodes.ComfyUI-ImageMetadataExtension.py.defs.ext.{module_name}"
     )
     module = importlib.import_module(package_name)
     CAPTURE_FIELD_LIST.update(getattr(module, "CAPTURE_FIELD_LIST", {}))
